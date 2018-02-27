c2eda9abf1a9a33e8750d29bbe4aac6a6589996c
SeonUkLee <scorpion022@naver.com>
2018-02-27 18:41:05 +0900

    Add second popup.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,6 +19,12 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello world");
+            MessageBox.Show("Second popup");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
